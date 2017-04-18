fb44819fffac4397fe499703e6abf3fb7088edac
junreturn1982 <ngochoang071982@gmail.com>
2017-04-18 21:50:59 +0700

    update objnest example json

--- 05DayArray/objnest.js
+++ 05DayArray/objnest.js
@@ -4,5 +4,25 @@ function Person(name,age,cash){
  this.getCash = function(){
      return cash;
  }
+ this.setCash = function(c){
+     if( c > 0)
+        cash = c;
+ }
 }
 
+function Receipt(id,maHoaDon){
+ this.id = id;
+ this.maHoaDon = maHoaDon;
+}
+
+let hoang = new Person('Hoang',35);
+
+let hoangReceipt = [];
+
+for (var i = 1; i < 5; i++) {
+    hoangReceipt.push(new Receipt(i,'HD0'+i)); 
+}
+
+hoang.receipt = hoangReceipt;
+
+console.log(JSON.stringify(hoang));
